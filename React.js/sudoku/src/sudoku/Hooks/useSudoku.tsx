e80d84574c6263081b267a1cb575e381aff04cff
Rflopz <devrafaelflc@gmail.com>
2024-01-08 12:31:40 -0700

    refactor to use hooks

---
+++ React.js/sudoku/src/sudoku/Hooks/useSudoku.tsx
@@ -0,0 +1,112 @@
+import { useCallback, useMemo, useState } from "react";
+import GridCell from "../components/GridCell";
+
+const getInitialGrid = () => {
+  return Array.from(Array(9), () =>
+    new Array(9).fill({ value: 0, enabled: true })
+  );
+};
+
+const initialPosition = "00";
+
+const useSudoku = () => {
+  const [grid, setGrid] = useState(getInitialGrid());
+  const [position, setPosition] = useState(initialPosition);
+
+  const isValid = useCallback(
+    (val: number, row: number, col: number) => {
+      if (val === 0) return true;
+
+      const findInRows = grid[row].filter(
+        (cell, i) => i !== col && cell.value === val
+      );
+
+      if (findInRows.length) return false;
+
+      for (let r = 0; r < grid.length; r++) {
+        if (r !== row && grid[r][col].value == val) return false;
+      }
+
+      return true;
+    },
+    [grid]
+  );
+
+  const handleOnChange = useCallback(
+    (row: number, col: number, val: number) => {
+      if (!Number.isNaN(val))
+        setGrid((current) => {
+          current[row][col] = {
+            ...current[row][col],
+            value: val,
+          };
+
+          return [...current];
+        });
+    },
+    []
+  );
+
+  const handleClearGrid = () => {
+    setGrid(getInitialGrid());
+    setPosition(initialPosition);
+  };
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const [x, y] = position.split("");
+      const row = parseInt(x);
+      const col = parseInt(y);
+
+      switch (e.key) {
+        case "j":
+        case "s":
+          if (row < grid.length - 1) setPosition([row + 1, col].join(""));
+          break;
+        case "k":
+        case "w":
+          if (row > 0) setPosition([row - 1, col].join(""));
+          break;
+        case "h":
+        case "a":
+          if (col > 0) setPosition([row, col - 1].join(""));
+          break;
+        case "l":
+        case "d":
+          if (col < grid[0].length - 1) setPosition([row, col + 1].join(""));
+          break;
+      }
+    },
+    [position, grid]
+  );
+
+  const GridMemo = useMemo<JSX.Element>(
+    () => (
+      <div className="grid">
+        {grid.map((row, x) =>
+          row.map((col, y) => (
+            <GridCell
+              key={`${x}_${y}`}
+              row={x}
+              isValid={isValid(col.value, x, y)}
+              focus={position}
+              setFocus={setPosition}
+              cell={y}
+              value={col.value}
+              onChange={handleOnChange}
+              onKeyDown={handleKeyDown}
+            />
+          ))
+        )}
+      </div>
+    ),
+    [grid, handleKeyDown, handleOnChange, isValid, position]
+  );
+
+  return {
+    GridMemo,
+    handleClearGrid,
+  };
+};
+
+export default useSudoku;
--- React.js/sudoku/src/sudoku/components/SudokuGrid.tsx
+++ React.js/sudoku/src/sudoku/components/SudokuGrid.tsx
@@ -1,99 +1,20 @@
-import { useCallback, useMemo, useState } from "react";
 import "./SudokuGrid.styles.css";
-import GridCell from "./GridCell";
-const initialGrid = Array.from(Array(9), () =>
-  new Array(9).fill({ value: 0, enabled: true })
-);
+import useSudoku from "../Hooks/useSudoku";
 
 const SudokuGrid = () => {
-  const [grid, setGrid] = useState(initialGrid);
-  const [focus, setFocus] = useState("00");
+  const { GridMemo, handleClearGrid } = useSudoku();
 
-  const isValid = useCallback(
-    (val: number, row: number, col: number) => {
-      if (val === 0) return true;
-
-      const inRow = grid[row].filter(
-        (cell, i) => i !== col && cell.value === val
-      );
-
-      if (inRow.length) return false;
-
-      for (let x = 0; x < 9; x++) {
-        if (x !== row && grid[x][col].value === val) return false;
-      }
-
-      return true;
-    },
-    [grid]
-  );
-
-  const handleOnChange = useCallback(
-    (row: number, col: number, val: number) => {
-      if (!Number.isNaN(val))
-        setGrid((current) => {
-          current[row][col] = {
-            ...current[row][col],
-            value: val || 0,
-          };
-          return [...current];
-        });
-    },
-    []
-  );
-
-  const handleKeyDown = useCallback(
-    (e: React.KeyboardEvent<HTMLInputElement>) => {
-      const pos = focus.split("");
-      const row = parseInt(pos[0]);
-      const col = parseInt(pos[1]);
-
-      console.log("move with " + e.key, focus);
-      switch (e.key) {
-        case "j":
-        case "s":
-          if (row < grid.length - 1) setFocus([row + 1, col].join(""));
-          break;
-        case "k":
-        case "w":
-          if (row > 0) setFocus([row - 1, col].join(""));
-          break;
-        case "h":
-        case "a":
-          if (col > 0) setFocus([row, col - 1].join(""));
-          break;
-        case "l":
-        case "d":
-          if (col < grid[0].length - 1) setFocus([row, col + 1].join(""));
-          break;
-      }
-    },
-    [focus, grid]
-  );
-
-  const GridMemoized = useMemo<JSX.Element>(() => {
-    return (
-      <div className="grid">
-        {grid.map((row, rowIndex) =>
-          row.map((cell, cellIndex) => (
-            <GridCell
-              key={`${rowIndex}_${cellIndex}`}
-              row={rowIndex}
-              isValid={isValid(cell.value, rowIndex, cellIndex)}
-              focus={focus}
-              setFocus={setFocus}
-              cell={cellIndex}
-              value={cell.value}
-              onChange={handleOnChange}
-              onKeyDown={handleKeyDown}
-            />
-          ))
-        )}
+  return (
+    <div className="container">
+      {GridMemo}
+      <div className="actions">
+        <button className="save">Save</button>
+        <button className="clear" onClick={handleClearGrid}>
+          Clear
+        </button>
       </div>
-    );
-  }, [focus, grid, handleKeyDown, handleOnChange, isValid]);
-
-  return <div className="container">{GridMemoized}</div>;
+    </div>
+  );
 };
 
 export default SudokuGrid;
